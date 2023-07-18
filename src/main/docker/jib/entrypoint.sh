9d3c6f42b3155c1ff24a4706f4844e0ff8dc110f
Winston Pidor <s2iwi2s@gmail.com>
2023-07-18 11:10:10 -0500

    debug

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 # exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.s2i.lms.LearnMngtSysApp"  "$@"
+ls -la
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp ./app/app.jar "com.s2i.lms.LearnMngtSysApp"  "$@"
