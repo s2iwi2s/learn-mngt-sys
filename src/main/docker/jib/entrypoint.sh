0257c17b2a3afe0ee76a0dd258a1a3dc169b306b
Winston Pidor <s2iwi2s@gmail.com>
2023-07-18 11:27:50 -0500

    use jar

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -2,6 +2,4 @@
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 # exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.s2i.lms.LearnMngtSysApp"  "$@"
-pwd
-ls -la
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp ./app.jar "com.s2i.lms.LearnMngtSysApp"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -jar ./app.jar "$@"
